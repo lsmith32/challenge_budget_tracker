db1aca6a5f2b012db060624b94b35d24df51b00d
lsmith32 <lmksmith97@gmail.com>
2022-04-18 20:18:50 -0500

    trouble shooting heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
@@ -25,5 +25,5 @@ mongoose.connect(MONGODB_URI, {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
